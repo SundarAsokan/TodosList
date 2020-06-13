2a6ff48dc25ee654232264220fbe610b968a445f
sundarasokan <sundarasokan@yahoo.com>
2020-06-13 18:55:17 -0400

    Updates

---
+++ precache-manifest.e3376cab7b875a9b5cd9c8b7e821f312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73831adbab898ca3a8a38087c63c8c14",
+    "url": "/TodosList/index.html"
+  },
+  {
+    "revision": "7ba5a194c99b47ede3b7",
+    "url": "/TodosList/static/js/2.2bc24df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodosList/static/js/2.2bc24df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf750c58b4c0eb9beda1",
+    "url": "/TodosList/static/js/main.91d9b811.chunk.js"
+  },
+  {
+    "revision": "e4f29601693ad6f090ac",
+    "url": "/TodosList/static/js/runtime-main.7b2afd8f.js"
+  }
+]);
